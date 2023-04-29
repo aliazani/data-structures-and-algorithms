e2c5b3429f2cc0819475dab56d31a63e326bbe5d
Mohammadali Azani <ali_azani@yahoo.com>
2023-04-29 13:56:21 +0330

    add documentation for LinkedListNode class.

--- src/main/java/io/github/aliazani/linear/linkedlist/LinkedListNode.java
+++ src/main/java/io/github/aliazani/linear/linkedlist/LinkedListNode.java
@@ -4,6 +4,13 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
+/**
+ * The LinkedListNode class represents a node in a linked list.
+ * <p>
+ * Each node contains a value of type N and a reference to the next node.
+ *
+ * @param <N> the type of value stored in the node
+ */
 @Getter
 @Setter
 @RequiredArgsConstructor
