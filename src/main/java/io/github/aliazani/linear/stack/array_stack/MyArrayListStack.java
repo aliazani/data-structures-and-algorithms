0b36041a67a4314b8089b8c17fa94d5d55dccd73
Mohammadali Azani <ali_azani@yahoo.com>
2023-05-03 22:53:07 +0330

    add java doc for MyArrayListStack.

--- src/main/java/io/github/aliazani/linear/stack/array_stack/MyArrayListStack.java
+++ src/main/java/io/github/aliazani/linear/stack/array_stack/MyArrayListStack.java
@@ -5,47 +5,82 @@ import io.github.aliazani.linear.stack.MyStack;
 import java.util.ArrayList;
 import java.util.EmptyStackException;
 
+/**
+ * A stack data structure implemented using an ArrayList to store the elements.
+ *
+ * @param <T> the type of elements stored in the stack, which must implement the Comparable interface
+ */
 public class MyArrayListStack<T extends Comparable<T>> implements MyStack<T> {
     private final ArrayList<T> array;
-    private int size;
 
+    /**
+     * Creates a new instance of MyArrayListStack.
+     */
     public MyArrayListStack() {
         array = new ArrayList<>();
     }
 
+    /**
+     * Adds an element to the top of the stack.
+     *
+     * @param item the element to add
+     */
     @Override
     public void push(T item) {
         array.add(item);
-        size++;
     }
 
+    /**
+     * Removes and returns the top element of the stack.
+     *
+     * @return the element that was on top of the stack
+     * @throws EmptyStackException if the stack is empty
+     */
     @Override
     public T pop() {
         if (isEmpty()) throw new EmptyStackException();
 
-        T top = array.get(--size);
-        array.remove(size);
-
-        return top;
+        return array.remove(size() - 1);
     }
 
+    /**
+     * Returns the top element of the stack without removing it.
+     *
+     * @return the element that is on top of the stack
+     * @throws EmptyStackException if the stack is empty
+     */
     @Override
     public T peek() {
         if (isEmpty()) throw new EmptyStackException();
 
-        return array.get(size - 1);
+        return array.get(size() - 1);
     }
 
+    /**
+     * Returns whether the stack is empty or not.
+     *
+     * @return true if the stack is empty, false otherwise
+     */
     @Override
     public boolean isEmpty() {
-        return size <= 0;
+        return array.isEmpty();
     }
 
+    /**
+     * Returns the number of elements in the stack.
+     *
+     * @return the number of elements in the stack
+     */
     @Override
     public int size() {
-        return size;
+        return array.size();
     }
 
+    /**
+     * Returns a string representation of the stack.
+     *
+     * @return a string representation of the stack
+     */
     @Override
     public String toString() {
         return array.toString();
