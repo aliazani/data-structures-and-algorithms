491ae8ac0b567c23d98be6493454c01594a64ba9
Mohammadali Azani <ali_azani@yahoo.com>
2023-05-20 16:41:07 +0330

    add java doc for StringReverser class.

--- src/main/java/io/github/aliazani/linear/stack/StringReverser.java
+++ src/main/java/io/github/aliazani/linear/stack/StringReverser.java
@@ -2,10 +2,20 @@ package io.github.aliazani.linear.stack;
 
 import java.util.Stack;
 
+/**
+ * Utility class for reversing a string using a stack.
+ */
 public class StringReverser {
     private StringReverser() {
     }
 
+    /**
+     * Reverses the input string.
+     *
+     * @param str the string to be reversed
+     * @return the reversed string
+     * @throws NullPointerException if the input string is null
+     */
     public static String reverseString(String str) {
         Stack<Character> stringStack = new Stack<>();
         StringBuilder reversed = new StringBuilder();
